e8351f2278667e5655e82e0bcd023932a81efa71
ashmind <ashmind@gmail.com>
2011-05-03 21:01:05 +0400

    Initial commit.

---
+++ Light.Interpreter/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Irony.Parsing;
+
+namespace Light.Interpreter {
+    public class Program {
+        public static void Main() {
+            var parser = new Parser(new LightGrammar());
+
+            while (InterpretLoop(parser)) { /* think about world */ }
+        }
+
+        private static bool InterpretLoop(Parser parser) {
+            var line = Console.ReadLine();
+            if (line == "exit")
+                return false;
+
+            var parsed = parser.Parse(line);
+            WriteMessages(parsed);
+            if (parsed.HasErrors())
+                return true;
+
+            var expression = (Expression)parsed.Root.AstNode;
+            var func = Expression.Lambda<Func<object>>(
+                Expression.Convert(expression, typeof(object))
+            ).Compile();
+            Console.WriteLine(func());
+
+            return true;
+        }
+
+        private static void WriteMessages(ParseTree parsed) {
+            foreach (var message in parsed.ParserMessages) {
+                if (message.Level == ParserErrorLevel.Error) {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                }
+                else if (message.Level == ParserErrorLevel.Warning) {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                }
+
+                Console.WriteLine(message.Message);
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ Light/LightGrammar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+using Irony.Parsing;
+
+namespace Light {
+    public class LightGrammar : Grammar {
+        public LightGrammar() {
+            var operators = new Dictionary<string, ExpressionType> {
+                { "+", ExpressionType.Add }
+            };
+
+            // Literals
+            var number = new NumberLiteral("number", NumberOptions.Default, (c, node) => node.AstNode = Expression.Constant(node.Token.Value));
+            
+            // NonTerminals
+            var expression = NonTerminal("expression", node => node.FirstChild.AstNode);
+            var binaryExpression = NonTerminal("binaryExpression", node => Expression.MakeBinary(
+                (ExpressionType)node.ChildNodes[1].AstNode,
+                (Expression)node.ChildNodes[0].AstNode,
+                (Expression)node.ChildNodes[2].AstNode
+            ));
+            var binaryOperator = NonTerminal("binaryOperator", node => operators[node.FindTokenAndGetText()]);
+
+            expression.Rule = number | binaryExpression;
+            binaryExpression.Rule = expression + binaryOperator + expression;
+            binaryOperator.Rule = operators.Keys.Select(k => (BnfExpression)ToTerm(k))
+                                                .Aggregate((a, b) => a | b);
+
+            this.Root = expression;
+            this.LanguageFlags = LanguageFlags.CreateAst | LanguageFlags.NewLineBeforeEOF;
+        }
+
+        private static NonTerminal NonTerminal(string name, Func<ParseTreeNode, object> getAstNode) {
+            return new NonTerminal(name, (c, node) => node.AstNode = getAstNode(node));
+        }
+    }
+}
