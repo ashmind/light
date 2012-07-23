8b2504f4db63ed573394080950f83075f69e5deb
Andrey Shchekin <ashmind@gmail.com>
2012-07-23 14:08:27 +1200

    Slightly improved presentation of [] and functions in Shell.

---
+++ Light.Tests/OfDescription/ObjectFormatterTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Light.Description;
+using MbUnit.Framework;
+
+namespace Light.Tests.OfDescription {
+    [TestFixture]
+    public class ObjectFormatterTests {
+        [Test]
+        [Row(new[] {1, 2}, "[1, 2]")]
+        [Row(new object[] { new[] { 1 }, 2 }, "[[1], 2]")]
+        public void Arrays(object array, string expectedResult) {
+            var result = new ObjectFormatter().Format(array);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void StaticDelegate() {
+            var result = new ObjectFormatter().Format((Func<double, TimeSpan>)(TimeSpan.FromMinutes));
+            Assert.AreEqual("<function:TimeSpan.FromMinutes>", result);
+        }
+    }
+}
--- Light/Description/ObjectFormatter.cs
+++ Light/Description/ObjectFormatter.cs
@@ -2,23 +2,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace Light.Description {
     public class ObjectFormatter {
         public string Format(object value) {
+            if (value is IList) {
+                var builder = new StringBuilder();
+                Append(builder, value);
+                return builder.ToString();
+            }
+
+            return FormatSimple(value);
+        }
+
+        private static string FormatSimple(object value) {
             var function = value as Delegate;
             if (function != null)
-                return "<function@" + function.Method.MethodHandle.Value + ">";
-
-            var list = value as IList;
-            if (list != null)
-                return "[" + string.Join(", ", list.Cast<object>()) + "]";
+                return "<function:" + function.Method.ReflectedType.Name + "." + function.Method.Name + ">";
 
             var @string = value as string;
             if (@string != null)
                 return "\"" + @string + "\"";
 
             return value.ToString();
+        }
+
+        private void Append(StringBuilder builder, object value) {
+            var list = value as IList;
+            if (list != null) {
+                builder.Append("[");
+                AppendAll(builder, ", ", list.Cast<object>());
+                builder.Append("]");
+                return;
+            }
+
+            builder.Append(FormatSimple(value));
+        }
+
+        // duplicates AppendAll in AstToStringTransformer. TODO: abstract
+        protected void AppendAll(StringBuilder builder, string delimiter, IEnumerable<object> values) {
+            var first = true;
+            foreach (var value in values) {
+                if (!first)
+                    builder.Append(delimiter);
+
+                Append(builder, value);
+                first = false;
+            }
         }
     }
 }
