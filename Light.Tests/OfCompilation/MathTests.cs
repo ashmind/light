39a1f7232d6c25552da7dbb23c6a61bcec1954ce
Andrey Shchekin <ashmind@gmail.com>
2012-07-23 21:33:24 +1200

    Added simple test on Math.

---
+++ Light.Tests/OfCompilation/MathTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Light.Tests.Helpers;
+using MbUnit.Framework;
+
+namespace Light.Tests.OfCompilation {
+    [TestFixture]
+    public class MathTests {
+        [Test]
+        [Row(10, 10)]
+        public void MathPow(double x, double y) {
+            var result = CompilationHelper.CompileAndEvaluate(string.Format("Math.Pow({0:0.00}, {1:0.00})", x, y));
+            Assert.AreEqual(Math.Pow(10, 10), result);
+        }
+    }
+}
